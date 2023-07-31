455c73ad4f9cf173393e971a8bacb3dd722231f9
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-07-31 02:18:30 +0000

    mobile design phase 2

--- src/components/ContactBox.jsx
+++ src/components/ContactBox.jsx
@@ -4,8 +4,8 @@ import ContactForm from "./ContactForm";
 function ContactBox() {
 	return (
 		<>
-			<div className="flex flex-col justify-center items-center">
-				<h1 className="text-[2.5rem] mb-5 text-p-white">Contact</h1>
+			<div className="grid grid-cols-1 justify-center items-center">
+				<h1 className="text-[2.5rem] mb-5 text-p-white text-center">Contact</h1>
 				<p className="text-base text-p-grey text-center mb-[3.12rem]">
 					I would love to hear about your project and how I could
 					help. Please fill in the form, and I’ll get back to you as
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,9 +5,9 @@ function ContactForm() {
 		<>
 			<div>
                 <form action="" className="flex flex-col gap-8 transparent">
-                    <input className=" bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" type="text" placeholder="NAME" />
-                    <input className=" bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" type="email" placeholder="EMAIL" />
-                    <textarea className="h-[6.6875rem] bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" name="" id="" cols="30" rows="10" placeholder="MESSAGE"></textarea>
+                    <input className=" focus:border-b-p-green bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" type="text" placeholder="NAME" />
+                    <input className=" focus:border-b-p-green bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" type="email" placeholder="EMAIL" />
+                    <textarea className="h-[6.6875rem] focus:border-b-p-green bg-p-dark-grey border-b-[0.0625rem] pt-0 pb-[1.06rem] px-6 text-base font-normal placeholder:font-normal text-p-white border-b-p-grey focus:outline-none focus-visible:ring-0" name="" id="" cols="30" rows="10" placeholder="MESSAGE"></textarea>
                     <div className="flex justify-end">
                         <button className="text-p-white pb-2 border-b-2 hover:text-p-green border-b-p-green" type="submit">SEND MESSAGE</button>
                     </div>
